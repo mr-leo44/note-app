7e945a24eb64bf108adc46bfa3f5d18dec8a4cdb
mr-leo44 <lionnelkaniki44@gmail.com>
2025-07-11 13:38:24 +0100

    feat: add department model and migration

---
+++ database/migrations/2025_07_11_115328_create_departments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Faculty;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('departments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('short_name');
+            $table->foreignIdFor(Faculty::class)->constrained();            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('departments');
+    }
+};
