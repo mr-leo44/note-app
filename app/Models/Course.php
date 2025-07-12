f8431ae493759dc91f03d2d03fb9195af28ce4a6
mr-leo44 <lionnelkaniki44@gmail.com>
2025-07-12 08:26:37 +0100

    feat: add model course

---
+++ app/Models/Course.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Promotion;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Course extends Model
+{
+    protected $fillable = [
+        'name',
+    ];
+
+    public function promotions(): BelongsToMany
+    {
+        return $this->belongsToMany(Promotion::class);
+    }
+}
