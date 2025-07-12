c48b693111a7ae84f98e4b0f8a9781d95279a6ab
mr-leo44 <lionnelkaniki44@gmail.com>
2025-07-12 08:28:39 +0100

    feat: update jury by relation

--- app/Models/Jury.php
+++ app/Models/Jury.php
@@ -3,13 +3,20 @@
 namespace App\Models;
 
 use App\Models\Account;
+use App\Models\Promotion;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Jury extends Model
 {
     public function account(): MorphOne
     {
         return $this->morphOne(Account::class, 'accountable');
+    }
+
+    public function promotions(): BelongsToMany
+    {
+        return $this->belongsToMany(Promotion::class);
     }
 }
