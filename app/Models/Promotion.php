bc6d2fec225ac9c87f6bb3a5c7aa0a82590bf1b1
mr-leo44 <lionnelkaniki44@gmail.com>
2025-07-12 08:28:56 +0100

    feat: update promotion by relation

--- app/Models/Promotion.php
+++ app/Models/Promotion.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use App\Models\Course;
 use App\Models\Department;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Promotion extends Model
 {
@@ -17,5 +19,10 @@ class Promotion extends Model
     public function department(): BelongsTo
     {
         return $this->belongsTo(Department::class);
+    }
+
+    public function promotions(): BelongsToMany
+    {
+        return $this->belongsToMany(Course::class);
     }
 }
