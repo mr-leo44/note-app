a412dc6d3f60867d7fae15d29b16269c3a44deae
mr-leo44 <lionnelkaniki44@gmail.com>
2025-07-12 08:27:05 +0100

    feat: add period model with relations

---
+++ app/Models/Period.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Result;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Period extends Model
+{
+    protected $fillable = [
+        'name',
+        'short_name',
+    ];
+
+    public function results(): HasMany
+    {
+        return $this->hasMany(Result::class);
+    }
+}
