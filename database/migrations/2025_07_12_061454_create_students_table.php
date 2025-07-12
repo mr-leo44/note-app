317ddf6d849fa00404056767c70621cd0776f62c
mr-leo44 <lionnelkaniki44@gmail.com>
2025-07-12 08:23:46 +0100

    feat: add student migration

---
+++ database/migrations/2025_07_12_061454_create_students_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Promotion;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('matricule')->unique();
+            $table->foreignIdFor(Promotion::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
