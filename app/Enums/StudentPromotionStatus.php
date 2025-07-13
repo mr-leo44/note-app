9099656f4fb7e31469a65ec353250f4041cf416d
mr-leo44 <lionnelkaniki44@gmail.com>
2025-07-13 23:46:47 +0100

    feat: create student_promotion pivot table

---
+++ app/Enums/StudentPromotionStatus.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Enums;
+
+enum StudentPromotionStatus: string
+{
+    case EN_COURS = 'en cours';
+    case DOUBLE = 'double';
+    case REUSSIE = 'reussie';
+
+    public function label(): string
+    {
+        return match($this) {
+            self::EN_COURS => 'En cours',
+            self::DOUBLE => 'Double',
+            self::REUSSIE => 'Réussie',
+        };
+    }
+}
