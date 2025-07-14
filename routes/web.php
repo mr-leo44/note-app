2a7ade365945c78f14117ea021f9042fb1a84f39
mr-leo44 <lionnelkaniki44@gmail.com>
2025-07-14 07:02:37 +0100

    feat: add departments routes

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FacultyController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DepartmentController;
 
 Route::get('/', function () {
     // Création automatique d'un admin si aucun user n'existe
@@ -42,6 +43,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+
 Route::resource('faculties', FacultyController::class)->only(['index', 'store', 'show','update','destroy'])->middleware('auth');
+Route::resource('departments', DepartmentController::class)->only(['index', 'store', 'show', 'update', 'destroy'])->middleware('auth');
 
 require __DIR__.'/auth.php';
