069c86e195e0c9deca028656fe8e1bb756983157
mr-leo44 <lionnelkaniki44@gmail.com>
2025-07-14 07:00:12 +0100

    update faculty

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -7,9 +7,12 @@ use App\Models\Promotion;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Department extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'name',
         'short_name',
