b37581735f00dfd74397b21554f6efbe3f72d9fa
mr-leo44 <lionnelkaniki44@gmail.com>
2025-07-14 05:05:18 +0100

    feat: add faculties links on navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('faculties.index')" :active="request()->routeIs('faculties.*')">
+                        {{ __('Facultés') }}
+                    </x-nav-link>
                 </div>
             </div>
 
