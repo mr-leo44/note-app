1a9cc30c5b0d4ee69adf58ae6ecdf257dfded21c
mr-leo44 <lionnelkaniki44@gmail.com>
2025-07-14 12:29:34 +0100

    feat: add show view

---
+++ resources/views/promotions/show.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h1 class="text-2xl font-bold">{{ $promotion->name }}</h1>
+    </x-slot>
+    <div class="container mx-auto py-8">
+        
+    </div>
+</x-app-layout>
