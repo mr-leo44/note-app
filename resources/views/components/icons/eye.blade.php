38c5d7fbc5df589dd4a66e9e835803aa23c1bb0d
mr-leo44 <lionnelkaniki44@gmail.com>
2025-07-14 06:59:26 +0100

    feat: add icons files

---
+++ resources/views/components/icons/eye.blade.php
@@ -0,0 +1,4 @@
+<svg class="w-6 h-6 text-gray-600 p-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M1.98 12.5C3.21 7.94 7.36 5 12 5c4.64 0 8.79 2.94 10.02 7.5a.98.98 0 0 1 0 .5C20.79 16.06 16.64 19 12 19c-4.64 0-8.79-2.94-10.02-7.5a.98.98 0 0 1 0-.5Z"/>
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+</svg>
---
+++ resources/views/components/icons/trash.blade.php
@@ -0,0 +1,4 @@
+<svg class="w-6 h-6 text-red-600 p-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6.75 7.5v-1A2.25 2.25 0 0 1 9 4.25h6A2.25 2.25 0 0 1 17.25 6.5v1m-10.5 0h13.5m-13.5 0v10.75A2.25 2.25 0 0 0 8.25 20.5h7.5a2.25 2.25 0 0 0 2.25-2.25V7.5m-13.5 0V6.5A2.25 2.25 0 0 1 6.75 4.25h10.5A2.25 2.25 0 0 1 19.5 6.5v1"/>
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M9.75 11.25v4.5m4.5-4.5v4.5"/>
+</svg>
