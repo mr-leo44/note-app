c7292fb4b704a8f3902172af31d2b754c2b1033f
mr-leo44 <lionnelkaniki44@gmail.com>
2025-07-14 20:55:11 +0100

    ajust slot

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
             @endisset
             
             <!-- Page Content -->
-            <main>
+            <main class="max-w-7xl mx-auto py-6 px-4 sm:px-6">
                 {{ $slot }}
             </main>
         </div>
