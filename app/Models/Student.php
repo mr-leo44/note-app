1ee0fc53d55700c1d8d12329bb37a6216ef1f43b
mr-leo44 <lionnelkaniki44@gmail.com>
2025-07-16 13:16:14 +0100

    update student model

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -5,11 +5,15 @@ namespace App\Models;
 use App\Models\Result;
 use App\Models\Promotion;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Student extends Model
 {
+    use HasFactory, SoftDeletes;
+    
     protected $fillable = [
         'name',
         'matricule',
