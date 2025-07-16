8ab6c4c096c102fcfa4f1ce4e7ba2d0356d13f43
mr-leo44 <lionnelkaniki44@gmail.com>
2025-07-16 07:57:37 +0100

    feat: initialize datatable globally

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,10 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import 'flowbite';
+import 'flowbite/dist/flowbite.turbo.js';
+import * as DataTable from 'simple-datatables';
+window.DataTable = DataTable.default || DataTable;
 
 window.Alpine = Alpine;
-
 Alpine.start();
