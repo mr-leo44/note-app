2565099c6ccea025efab76028a72e6b706267516
mr-leo44 <lionnelkaniki44@gmail.com>
2025-07-17 18:51:01 +0100

    feat: add published_by column to results table

---
+++ database/migrations/2025_07_17_155938_add_published_by_column_to_result_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('results', function (Blueprint $table) {
+            $table->unsignedBigInteger('published_by')->nullable();
+            $table->foreign('published_by')->references('id')->on('users');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('result', function (Blueprint $table) {
+            //
+        });
+    }
+};
