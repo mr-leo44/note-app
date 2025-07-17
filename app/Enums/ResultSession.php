a05ed01efaf0dd74b34896b5dc78c05bebdbc067
mr-leo44 <lionnelkaniki44@gmail.com>
2025-07-17 22:32:12 +0100

    feat: resultSezssion enum

---
+++ app/Enums/ResultSession.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Enums;
+
+enum ResultMSession: string
+{
+    case S1 = 'Première Session';
+    case S2 = 'Deuxième Session';
+
+    public function label(): string
+    {
+        return match($this) {
+            self::S1 => 'Première Session',
+            self::S2 => 'Deuxième Session',
+        };
+    }
+}
