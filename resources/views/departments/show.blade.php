ef51ae9b8dd26ea75753d83e63811441205461d6
mr-leo44 <lionnelkaniki44@gmail.com>
2025-07-26 07:04:04 +0100

    perf: update show department by adding some column

--- resources/views/departments/show.blade.php
+++ resources/views/departments/show.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex justify-between items-center">
             <div class="flex items-center gap-2">
-                <a href="{{ route('departments.index') }}"
+                <a href="{{ url()->previous() }}"
                     class="inline-flex items-center px-3 py-2 text-sm font-medium text-blue-700 bg-blue-100 hover:bg-blue-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-300">
                     <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
@@ -53,16 +53,24 @@
                         <th scope="col" class="px-6 py-3">#</th>
                         <th scope="col" class="px-6 py-3">Nom</th>
                         <th scope="col" class="px-6 py-3">Abbreviations</th>
+                        <th scope="col" class="px-6 py-3">Nbre Etudiants</th>
+                        <th scope="col" class="px-6 py-3">Jury</th>
                         <th scope="col" class="px-6 py-3">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($promotions as $promotion)
+                    @php
+                        $juryByPromotion = $promotion->juries()->where('promotion_id', $promotion->id)->wherePivot('deleted_at', null)->first();
+                        $jury = $juryByPromotion ? \App\Models\Account::where('accountable_id', $juryByPromotion->id)->first()->user : null;
+                    @endphp
                         <tr
                             class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100 dark:hover:bg-gray-900 transition">
                             <td class="px-6 py-4">{{ $loop->iteration }}</td>
                             <td class="px-6 py-4 font-semibold">{{ $promotion->name }}</td>
                             <td class="px-6 py-4">{{ $promotion->short_name }}</td>
+                            <td class="px-6 py-4">{{ $promotion->students()->count() }}</td>
+                            <td class="px-6 py-4">{{ $jury->name }}</td>
                             <td class="px-6 py-4 flex gap-2">
                                 <a href="{{ route('promotions.show', $promotion) }}"
                                     class="bg-gray-100 hover:bg-gray-200 p-1.5 rounded" title="Voir">
