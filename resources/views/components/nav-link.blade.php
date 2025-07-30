d553a738b4d2665ca8f1728c00ecb80a9e734663
mr-leo44 <lionnelkaniki44@gmail.com>
2025-07-30 23:05:19 +0100

    refactor: update nav-link background

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -3,7 +3,7 @@
 @php
     $classes =
         $active ?? false
-            ? 'flex items-center p-2 bg-gray-100 dark:bg-gray-700 text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-800 group transition duration-150 ease-in-out'
+            ? 'flex items-center p-2 bg-blue-700 rounded-lg text-white hover:bg-blue-800 group transition duration-150 ease-in-out'
             : 'flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group transition duration-150 ease-in-out';
 @endphp
 
