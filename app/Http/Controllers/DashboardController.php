205ef9dd6ec09c0e646e6c94c923b2aee3699178
mr-leo44 <lionnelkaniki44@gmail.com>
2025-08-01 09:46:42 +0100

    feat: [ref] last commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use App\Models\Jury;
+use App\Models\Course;
+use App\Models\Period;
+use App\Models\Result;
+use App\Models\Promotion;
+use App\Models\ResultStatus;
+use App\Models\ResultSession;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
+
+class DashboardController extends Controller
+{
+    public function dashboard()
+    {
+        Carbon::setLocale('fr');
+        $currentPeriod = Period::where('current', true)->first();
+        $currentSession = $currentPeriod ? ResultSession::where('current', true)->where('period_id', $currentPeriod->id)->first() : null;
+        $promotions = Promotion::paginate();
+        $courses = Course::all();
+        $juries = Jury::all();
+        $students = $currentPeriod ? DB::table('promotion_student')->where('period', $currentPeriod->name)->where('status', 'en cours')->get() : null;
+        $publishedResultByPromotion = 0;
+        if ($currentSession) {
+            foreach ($promotions as $promotion) {
+                $studentsByPromotion = $promotion
+                    ->students()
+                    ->wherePivot('promotion_id', $promotion->id)
+                    ->wherePivot('period', $currentPeriod->name)
+                    ->wherePivot('status', 'en cours')
+                    ->get();
+                foreach ($studentsByPromotion as $student) {
+                    $studentResult = \App\Models\Result::where('student_id', $student->id)
+                        ->where('result_session_id', $currentSession->id)
+                        ->first();
+                    if ($studentResult) {
+                        $publishedResultByPromotion++;
+                    }
+                }
+    
+                $resultStatus =
+                    ResultStatus::where('promotion_id', $promotion->id)
+                    ->where('session', $currentSession->id)
+                    ->first();
+                $promotion['statusOfResult'] = $resultStatus;
+            }
+        }
+
+        return view('dashboard', compact('currentPeriod', 'currentSession', 'promotions', 'courses', 'juries', 'students', 'publishedResultByPromotion'));
+    }
+}
