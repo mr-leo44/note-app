4284b76f373b497abab5e540f00fa9b4f385d62a
mr-leo44 <lionnelkaniki44@gmail.com>
2025-08-01 17:16:20 +0100

    feat: config pwa

---
+++ config/pwa.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Would you like the install button to appear on all pages?
+      Set true/false
+    |--------------------------------------------------------------------------
+    */
+
+    'install-button' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | PWA Manifest Configuration
+    |--------------------------------------------------------------------------
+    |  php artisan erag:pwa-update-manifest
+    */
+
+    'manifest' => [
+        'name' => 'Isipa Note App',
+        'short_name' => 'Notes',
+        'background_color' => '#ffffff',
+        'display' => 'standalone',
+        'description' => 'Application pour verifier ses resultats à l\'ISIPA.',
+        'theme_color' => '#3367D6',
+        'icons' => [
+            [
+                'src' => 'logo.png',
+                'sizes' => '512x512',
+                'type' => 'image/png',
+            ],
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Debug Configuration
+    |--------------------------------------------------------------------------
+    | Toggles the application's debug mode based on the environment variable
+    */
+
+    'debug' => env('APP_DEBUG', false),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Livewire Integration
+    |--------------------------------------------------------------------------
+    | Set to true if you're using Livewire in your application to enable
+    | Livewire-specific PWA optimizations or features.
+    */
+
+    'livewire-app' => false,
+];
