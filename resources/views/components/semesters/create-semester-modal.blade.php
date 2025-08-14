6259836184e207bdbfe4c457424b6ffd17984c38
mr-leo44 <lionnelkaniki44@gmail.com>
2025-08-14 07:52:41 +0100

    feat: add semester component for crud

---
+++ resources/views/components/semesters/create-semester-modal.blade.php
@@ -0,0 +1,32 @@
+<div id="createSemesterModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full flex justify-center items-center">
+    <div class="relative p-4 w-full max-w-md h-full md:h-auto">
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-hide="createSemesterModal">
+                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                <span class="sr-only">Fermer</span>
+            </button>
+            <div class="p-6">
+                <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Créer une nouvelle période</h3>
+                <form method="POST" action="{{ route('semesters.store') }}">
+                    @csrf
+                    <div class="mb-4">
+                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom</label>
+                        <select name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-sky-500 focus:border-sky-500 block w-full p-2.5 pr-10 appearance-none required">
+                            <option value="">Sélectionner un semestre</option>
+                            @foreach(App\Enums\ResultSemester::cases() as $semester)
+                                <option value="{{ $semester->value }}" :selected="semester == {{ $semester->label() }}">{{ $semester->label() }}</option>
+                            @endforeach
+                        </select>
+                        @error('name')<span class="text-red-600 text-xs">{{ $message }}</span>@enderror
+                    </div>
+                    
+                    <div class="mb-4 flex items-center">
+                        <input type="checkbox" name="current" id="current" value="1" class="mr-2" {{ old('current') ? 'checked' : '' }}>
+                        <label for="current" class="text-sm text-gray-900 dark:text-white">En cours</label>
+                    </div>
+                    <button type="submit" class="w-full text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Créer</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
