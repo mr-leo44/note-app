e00646f70241c377f4b9789d44fd20ad0e028ea3
mr-leo44 <lionnelkaniki44@gmail.com>
2025-08-15 16:41:47 +0100

    refactor: update active bg color

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -3,7 +3,7 @@
 @php
     $classes =
         $active ?? false
-            ? 'flex items-center p-2 bg-sky-700 rounded-lg text-white hover:bg-sky-800 group transition duration-150 ease-in-out'
+            ? 'flex items-center p-2 bg-gray-100 rounded-lg hover:bg-gray-200 group transition duration-150 ease-in-out'
             : 'flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group transition duration-150 ease-in-out';
 @endphp
 
