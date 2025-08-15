32c33967c5bac4fcf25293772ff975fec65a052a
mr-leo44 <lionnelkaniki44@gmail.com>
2025-08-15 16:56:11 +0100

    feat: add course category components

---
+++ resources/views/components/course-categories/edit-course-category-modal.blade.php
@@ -0,0 +1,40 @@
+<!-- Modal d'édition pour une section -->
+@props(['course_category'])
+<div id="editCourseCategoryModal-{{ $course_category->id }}" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full flex justify-center items-center">
+    <div class="relative p-4 w-full max-w-md h-full md:h-auto">
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-hide="editCourseCategoryModal-{{ $course_category->id }}">
+                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                <span class="sr-only">Fermer</span>
+            </button>
+            <div class="p-6">
+                <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Modifier la section</h3>
+                <form method="POST" action="{{ route('course-categories.update', $course_category) }}">
+                    @csrf
+                    @method('PUT')
+                    <div class="mb-4">
+                        <label for="name-{{ $course_category->id }}" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom</label>
+                        <input type="text" name="name" id="name-{{ $course_category->id }}" value="{{ old('name', $course_category->name) }}" autocomplete="off" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-sky-500 focus:border-sky-500 block w-full p-2.5 @error('name') border-red-500 @enderror" required>
+                        @error('name')<span class="text-red-600 text-xs">{{ $message }}</span>@enderror
+                    </div>
+                    <div class="mb-4">
+                        <label for="short_name-{{ $course_category->id }}" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Abréviation</label>
+                        <input type="text" name="short_name" id="short_name-{{ $course_category->id }}" value="{{ old('short_name', $course_category->short_name) }}" autocomplete="off" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-sky-500 focus:border-sky-500 block w-full p-2.5 @error('short_name') border-red-500 @enderror" required>
+                        @error('short_name')<span class="text-red-600 text-xs">{{ $message }}</span>@enderror
+                    </div>
+                    <div class="mb-4">
+                        <label for="ue-{{ $course_category->id }}" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Crédit UE</label>
+                        <input type="number" min="1" max="30" id="ue-{{ $course_category->id }}" name="ue" value="{{ old('ue', $course_category->ue) }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-sky-500 focus:border-sky-500 block w-full p-2.5 @error('name') border-red-500 @enderror" required autocomplete="off">
+                        @error('ue')<span class="text-red-600 text-xs">{{ $message }}</span>@enderror
+                    </div>
+                    <div class="mb-4">
+                        <label for="category_alias-{{ $course_category->id }}" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Catégorie UE</label>
+                        <input type="text" name="category_alias" id="category_alias-{{ $course_category->id }}" value="{{ old('category_alias', $course_category->category_alias) }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-sky-500 focus:border-sky-500 block w-full p-2.5 @error('short_name') border-red-500 @enderror" autocomplete="off">
+                        @error('category_alias')<span class="text-red-600 text-xs">{{ $message }}</span>@enderror
+                    </div>
+                    <button type="submit" class="w-full text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Modifier</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
