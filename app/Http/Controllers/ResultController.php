9fb8e56b8089e0c3115b97df5cf87739df38ff0d
mr-leo44 <lionnelkaniki44@gmail.com>
2025-08-21 09:59:08 +0100

    feat: add method search on result controller

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -2,23 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use App\Enums\ResultByPromotionStatus;
+use App\Models\Result;
+use App\Models\Student;
+use App\Models\ResultStatus;
 use Illuminate\Http\Request;
 use App\Models\ResultSession;
-use App\Models\ResultStatus;
+use App\Enums\ResultByPromotionStatus;
+use App\Models\Semester;
 
 class ResultController extends Controller
 {
     public function index()
     {
         $currentSession = ResultSession::where('current', true)->first();
-        $publications = ResultStatus::
-        where('session', $currentSession->id)->where('status', '!=', ResultByPromotionStatus::DRAFT->value)->
-        paginate(10);
+        $publications = ResultStatus::where('session', $currentSession->id)->where('status', '!=', ResultByPromotionStatus::DRAFT->value)->paginate(10);
         return view('publications.index', compact('publications'));
     }
 
-public function onlinePublishResults(Request $request)
+    public function onlinePublishResults(Request $request)
     {
         $currentSession = ResultSession::where('current', true)->first();
         $completedResults = ResultStatus::where('session', $currentSession->id)
@@ -30,5 +31,26 @@ public function onlinePublishResults(Request $request)
             ]);
         }
         return response()->json();
+    }
+
+    public function search(Request $request)
+    {
+        // dd($request->all());
+        $request->validate([
+            'matricule' => 'required|string',
+            'semester' => 'required|integer|exists:semesters,id',
+        ]);
+
+        $student = Student::where('matricule', $request->matricule)->firstOrFail();
+        if (!$student) {
+            return back()->with('error', 'Étudiant non trouvé');
+        } else {
+            $currentPromotion = $student->promotions()
+                ->where('status', 'en cours')
+                ->first();
+        }
+        $semester = Semester::findOrFail($request->semester);
+        // dd($semester, $currentPromotion, $student);
+        return view('results', compact(['student', 'currentPromotion', 'semester']));
     }
 }
