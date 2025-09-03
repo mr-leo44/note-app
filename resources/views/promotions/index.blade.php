64bc1dfc41e798d0cb527a7c773f5479aaaca3d7
mr-leo44 <lionnelkaniki44@gmail.com>
2025-09-03 12:46:13 +0100

    feat: reaffect edit promotion

--- resources/views/promotions/index.blade.php
+++ resources/views/promotions/index.blade.php
@@ -1,11 +1,12 @@
 <x-app-layout>
+    @php
+        $departmentsCount = \App\Models\Department::count();
+        $isAdmin = auth()->user()->account->accountable_type === \App\Models\Admin::class;
+    @endphp
     <x-slot name="header">
         <div class="flex justify-between items-center py-2">
             <h1 class="text-base md:text-2xl font-bold">Liste des promotions</h1>
-            @php
-                $departmentsCount = \App\Models\Department::count();
-            @endphp
-            @if (auth()->user()->account->accountable_type === \App\Models\Admin::class)
+            @if ($isAdmin)
                 <button id="openModalBtn" data-modal-target="createPromotionModal" data-modal-toggle="createPromotionModal"
                     class="@if($departmentsCount === 0) hidden @endif text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                     type="button" title="Ajouter une promotion">
@@ -68,20 +69,22 @@
                                     class="bg-gray-100 hover:bg-gray-200 p-1.5 rounded" title="Voir">
                                     <x-icons.eye />
                                 </a>
-                                {{-- <button type="button" class="bg-sky-100 hover:bg-sky-200 p-1.5 rounded"
+                                @if ($isAdmin)    
+                                <button type="button" class="bg-sky-100 hover:bg-sky-200 p-1.5 rounded"
                                     title="Modifier" data-promotion-id="{{ $promotion->id }}"
                                     data-modal-target="editPromotionModal-{{ $promotion->id }}"
                                     data-modal-toggle="editPromotionModal-{{ $promotion->id }}">
                                     <x-icons.pencil-square />
-                                </button> --}}
+                                </button>
                                 <button type="button" class="bg-red-100 hover:bg-red-200 p-1.5 rounded"
                                     title="Supprimer" data-promotion-id="{{ $promotion->id }}"
                                     data-action-url="{{ route('promotions.destroy', $promotion) }}"
                                     data-modal-target="deletePromotionModal" data-modal-toggle="deletePromotionModal">
                                     <x-icons.trash />
                                 </button>
-                                {{-- <x-promotions.edit-promotion-modal :promotion="$promotion" :departments="$promotions->pluck('department')->unique('id')->filter()->values()" /> --}}
+                                <x-promotions.edit-promotion-modal :promotion="$promotion" :departments="$promotions->pluck('department')->unique('id')->filter()->values()" />
                                 <x-promotions.delete-promotion-modal />
+                                @endif
                             </td>
                         </tr>
                     @endforeach
